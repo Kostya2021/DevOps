6c5a1dc9895b14192b351e8481514d5d3c30a9ba
Kostya Liakh <lyahk@mail.ru>
2021-04-16 21:36:02 +0200

    Gradle!

--- src/main/java/myproject/Greeter.java
+++ src/main/java/myproject/Greeter.java
@@ -1,10 +1,12 @@
 package myproject;
 public class Greeter {
     String name;
+
     public Greeter(String name) {
         this.name = name;
     }
+
     public String greet() {
-        return "Hello " + name + ", nice to meet you!");
+        return "Hello " + name + ", nice to meet you!";
     }
 }
